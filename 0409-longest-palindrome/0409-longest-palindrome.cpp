20901c18fcfad9b3e817780748cb25cb0b11db11
Kunalkhuteta <154576674+Kunalkhuteta@users.noreply.github.com>
2024-06-04 07:15:01 +0530

    Time: 4 ms (44.24%), Space: 7.8 MB (75.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-     int cnt[128]{};
-     for(char c:s)   
-     {
-        ++cnt[c];
-     }
-     int ans=0;
-     for(int v:cnt)
-     {
-        ans+=v-(v&1);
-        if(ans%2==0 && v%2==1)
-            ++ans;
-     }
-     return ans;
+        vector<int> lower(26, 0);
+        vector<int> upper(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'a') {
+                lower[s[i] - 'a']++;
+            } else {
+                upper[s[i] - 'A']++;
+            }
+        }
+        int c = 0;
+        int f = 0;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] % 2 == 0) {
+                c += lower[i];
+            } else {
+                c += lower[i] - 1;
+                f = 1;
+            }
+            if (upper[i] % 2 == 0) {
+                c += upper[i];
+            } else {
+                c += upper[i] - 1;
+                f = 1;
+            }
+        }
+        return c + f;
     }
 };
